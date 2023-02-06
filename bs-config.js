1f5016bf47b350857452c867f08cc396349a9b12
beginor <beginor@qq.com>
2023-02-06 15:47:08 +0800

    add failback middleware

--- bs-config.js
+++ bs-config.js
@@ -1,4 +1,18 @@
 /** Browser Sync Config */
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+
+const baseDir = __dirname;
+const indexFile = 'index.html'
+
+// failback rules;
+const failbackRules = [
+  // { pattern: '/apps/(?!assets/).*/', failback: '/apps/index.html' },
+  { pattern: '/(?!assets/).*/', failback: '/index.html' },
+];
+
+/** @type {import('browser-sync').Options} */
 module.exports = {
   /**  files to watch */
   files: [
@@ -6,9 +20,26 @@ module.exports = {
     'index.html'
   ],
   server: {
-    baseDir: './',
+    baseDir,
     directory: true,
-    index: 'index.html'
+    index: indexFile,
+    middleware: function(req, res, next) {
+      const uri = url.parse(req.url);
+      if (!fs.existsSync(baseDir + uri.pathname)) {
+        for (const rule of failbackRules) {
+          const regex = new RegExp(rule.pattern);
+          if (regex.test(req.url)) {
+            let url = rule.failback;
+            if (uri.search) {
+              url += uri.search;
+            }
+            req.url = url;
+            break;
+          }
+        }
+      }
+      next();
+    }
   },
   open: false,
   cors: true
