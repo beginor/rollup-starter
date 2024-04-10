4a48fc07b9d424a068afe2694ba6c3045f96bb9a
beginor <beginor@qq.com>
2024-04-10 11:16:28 +0800

    remove unused plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import fs from 'fs';
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // fallback rules;
 const fallbackRules = [
@@ -29,7 +28,6 @@ export default defineConfig({
     }
   },
   plugins: [
-    react(),
     spaFallbackPlugin(),
   ]
 });
